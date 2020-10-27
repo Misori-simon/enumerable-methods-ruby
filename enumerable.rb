c5706d9b96eea93077eb9160371bf51d2a683173
Misori-simon <misorisimon@gmail.com>
2020-10-27 16:46:58 +0100

    add the my_select method -return to enum for empty block -looped through the elements and returned those that yielded true

--- enumerable.rb
+++ enumerable.rb
@@ -2,7 +2,7 @@ module Enumerable
 
   def my_each
     return to_enum(:my_each) unless block_given?
-    
+
     array = self.to_a
     for element in array
       yield element
@@ -10,16 +10,30 @@ module Enumerable
     self
   end
 
-  def my_each_with_index 
-    return to_enum(:my_each) unless block_given? 
-    
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
 
     array = self.to_a
     (array.length).times  do |x|
       yield array[x], x
     end
+    self
   end
-  self
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    array = self
+    final_array = []
+    for element in array
+      final_array << element if yield element
+    end
+    final_array
+  end
 end
 
-
+arr = {"messi" => 54, "ronaldo" => 55, "zidane" => 53, "fowler" => 78 }
+p arr
+p arr.select { |num, y|  num == "messi"  }
+p arr.my_select { |num, y|  num == "messi"  }
