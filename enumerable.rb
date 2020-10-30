7dd6f1c05b9182c647d7bf816dfc9bf5f8cca14b
Misori-simon <misorisimon@gmail.com>
2020-10-30 09:40:58 +0100

    fix rubocop errors, update readme -fixed some rubocop errors -added content to the readme file

--- enumerable.rb
+++ enumerable.rb
@@ -1,10 +1,17 @@
-module Enumerable
+# rubocop: disable Metrics/ModuleLength
+# rubocop: disable Metrics/MethodLength
+# rubocop: disable Metrics/CyclomaticComplexity
+# rubocop: disable Metrics/PerceivedComplexity
+# rubocop: disable Style/IdenticalConditionalBranches
+# rubocop: disable Style/CaseEquality
+# rubocop: disable Style/ExplicitBlockArgument
 
+module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    array = self.to_a
-    for element in array
+    array = to_a
+    array.each do |element|
       yield element
     end
     self
@@ -13,9 +20,8 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
-
-    array = self.to_a
-    (array.length).times  do |x|
+    array = to_a
+    array.length.times do |x|
       yield array[x], x
     end
     self
@@ -25,45 +31,43 @@ module Enumerable
     return to_enum(:my_select) unless block_given?
 
     array = self
-    final_object = []   if self.class == Array
-    final_object = {}   if self.class == Hash
-    for element in array
-      if final_object.class == Array
-       final_object << element if yield element
-      else
-        final_object[element[0]] = element[1] if yield element
+    array = to_a if instance_of?(Range)
+    final_object = [] if array.instance_of?(Array)
+    final_object = {} if array.instance_of?(Hash)
+    array.each do |element|
+      if final_object.instance_of?(Array)
+        final_object << element if yield element
+      elsif yield element
+        final_object[element[0]] = element[1]
       end
-
     end
     final_object
   end
 
   def my_all?(pattern = nil)
-
     unless block_given? || pattern
-     return (self.my_any? {|x| x == false}) ? false : true
+      return my_any? { |x| x == false } ? false : true
     end
 
-    result =  true
+    result = true
 
     if pattern
-     if pattern.is_a?(Regexp)
-      self.my_each {|element| result = false unless element.match(pattern)}
-      return result
-     else
-      self.my_each {|element| result = false unless pattern === element}
-      return result
-     end
+      if pattern.is_a?(Regexp)
+        my_each { |element| result = false unless element.match(pattern) }
+        return result
+      else
+        my_each { |element| result = false unless pattern === element }
+        return result
+      end
     end
 
-    self.my_each {|element| result = false unless yield element }
-    return result
+    my_each { |element| result = false unless yield element }
+    result
   end
 
   def my_any?(pattern = nil)
-
     unless block_given? || pattern
-      self.my_each do |element|
+      my_each do |element|
         return element ? true : false
       end
     end
@@ -71,124 +75,114 @@ module Enumerable
     result = false
 
     if pattern
-     if pattern.is_a?(Regexp)
-      self.my_each  do |element| 
-        return true if element.match(pattern) 
-      end
-      return false
+      if pattern.is_a?(Regexp)
+        my_each do |element|
+          return true if element.match(pattern)
+        end
+        return false
 
-     else
-      self.my_each do |element| 
-        return true if pattern === element
+      else
+        my_each do |element|
+          return true if pattern === element
+        end
+        return false
       end
-      return false
-     end
     end
 
-    for element in self
-      result = true if  yield element
+    each do |element|
+      result = true if yield element
     end
     result
   end
 
   def my_none?(pattern = nil)
+    unless block_given? || pattern
+      return true unless any? { |element| element == true }
 
-    unless block_given? || pattern 
-      return true unless self.any? {|element| element == true}
       return false
     end
 
     if pattern
-     if pattern.is_a?(Regexp)
-      self.my_each  do |element| 
-        return false if element.match(pattern) 
-      end
-      return true
+      if pattern.is_a?(Regexp)
+        my_each do |element|
+          return false if element.match(pattern)
+        end
+        return true
 
-     else
-      self.my_each do |element| 
-        return false if pattern === element
+      else
+        my_each do |element|
+          return false if pattern === element
+        end
+        return true
       end
-      return true
-     end
     end
 
-    for element in self
+    each do |element|
       return false if yield element
     end
-    return true
-
+    true
   end
 
   def my_count(arg = nil)
-    return self.length if !(block_given?) && arg == nil
+    return length if !block_given? && arg.nil?
+
     counter = 0
-    for element in self
+    each do |element|
       if arg
-        if arg == element
-          counter +=1
-        end
+        counter += 1 if arg == element
       elsif yield element
-        counter +=1
+        counter += 1
       end
     end
-    return counter
+    counter
   end
 
   def my_map(&proc)
     return to_enum(:map) unless block_given?
 
     final_object = []
-    for element in self
+    each do |element|
       final_object << (proc.call(element))
     end
     final_object
   end
 
+  def my_inject(accumulator = nil, symbol = nil, &block)
+    raise LocalJumpError, 'no block given?' if accumulator.nil? && symbol.nil? && block.nil?
 
-def my_inject(accumulator = nil, symbol = nil, &block)
+    raise LocalJumpError, 'provide either a symbol or a block' if symbol && block
 
-  if accumulator.nil? && symbol.nil? && block.nil?
-    raise LocalJumpError, "no block given?"
-  end
-
-  if symbol && block
-    raise LocalJumpError, "provide either a symbol or a block"
-  end
-
-  if symbol.nil? && block.nil?
-    symbol = accumulator
-    accumulator = nil
-  end
-
-  array = self.to_a
-
-  code_to_run = case symbol
-    when Symbol
-      lambda { |accumulator, element| accumulator.send(symbol, element) }
-    when nil
-      block
-  end
-
-  if accumulator.nil?
-    ignore_first_element = true
-    accumulator = array[0]
-  end
-
-  switch = 0
-
-  each do |element|
-    unless ignore_first_element && switch == 0
-      accumulator = code_to_run.call(accumulator, element)
+    if symbol.nil? && block.nil?
+      symbol = accumulator
+      accumulator = nil
     end
-    switch += 1
-  end
-  accumulator
-end
 
+    array = to_a
+
+    code_to_run = case symbol
+                  when Symbol
+                    ->(acc, element) { acc.send(symbol, element) }
+                  when nil
+                    block
+                  end
+
+    if accumulator.nil?
+      ignore_first_element = true
+      accumulator = array[0]
+    end
+
+    switch = 0
+
+    each do |element|
+      accumulator = code_to_run.call(accumulator, element) unless ignore_first_element && switch.zero?
+      switch += 1
+    end
+    accumulator
+  end
 end
 
 def multiply_els(arr)
   arr.my_inject(:*)
 end
 
+# rubocop: enable all
